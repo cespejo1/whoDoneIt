cd54ae99a4162abcaebdfe5df78f3a143dc2b68c
Carlos Espejo <cespejo1@gmail.com>
2023-02-25 17:43:39 -0600

    Cleaned code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Person import Person
-# print("this will be bigger than 💥 Call of Duty 💥")
+
 
 #This class will be the class that will load the game and run the game.
 #The game will have a loop which will keep the game running. I'm thinking
@@ -8,15 +8,15 @@ from Person import Person
 
 #initial setup
 p1 = Person("John", 36)
-
-
-#This will be the Game loop. 
 gameIsOver = False
-userName = input("Welcome to the greatest game you'll ever play. To start off, what is the name of your character?\n")
 
+ 
+
+userName = input("Welcome to the greatest game you'll ever play. To start off, what is the name of your character?\n")
 print("Enjoy the journey, ", userName, "!")
 print("If at anytime you would like to quit the game, enter q or Q or quit when asked for input")
 
+#This will be the Game loop.
 while gameIsOver == False:
   userInput = input("What would you like to do?")
   if(userInput == "q" or userInput == "Q" or userInput == "quit"):
