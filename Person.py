c83f05f53b4058005b835aa65d87de2856a7f598
Carlos Espejo <cespejo1@gmail.com>
2023-02-25 17:41:54 -0600

    Added a game loop

--- Person.py
+++ Person.py
@@ -1,5 +1,24 @@
+#This class will represent the character the user will be. We will add attributes 
+# here that we want the user to have and change throughout the game
+
+
+
 class Person:
-  def __init__(self, name, age):
+  def __init__(self, name, health):
     self.name = name
-    self.age = age
+    self.health = health
+
+# getter method
+  def get_health(self):
+      return self.h
+      
+# setter method
+  def set_health(self, x):
+      self.health = x
+
+  def reduce_health(self, x):
+      self.health = self.health - x
+
+  def add_health(self, x):
+      self.health = self.health + x
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from Person import Person
 # print("this will be bigger than 💥 Call of Duty 💥")
 
+#This class will be the class that will load the game and run the game.
+#The game will have a loop which will keep the game running. I'm thinking
+#we build the game from the ground up and make the game start off simple and 
+#we slowly add more and more.
 
+#initial setup
 p1 = Person("John", 36)
 
 
@@ -10,5 +15,14 @@ gameIsOver = False
 userName = input("Welcome to the greatest game you'll ever play. To start off, what is the name of your character?\n")
 
 print("Enjoy the journey, ", userName, "!")
+print("If at anytime you would like to quit the game, enter q or Q or quit when asked for input")
+
+while gameIsOver == False:
+  userInput = input("What would you like to do?")
+  if(userInput == "q" or userInput == "Q" or userInput == "quit"):
+    gameIsOver = True
+    print("Thanks for playing!")
+
+
 
 
