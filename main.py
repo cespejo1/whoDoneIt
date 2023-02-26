81eb79240219271a0f906389d0170cfd63a2f4aa
stellernate <stellernate@gmail.com>
2023-02-26 15:49:27 -0600

    give this a run
    not actually how the story will start just wanted to test out a couple of things

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ gameIsOver = False
 
 userName = input(
     "Welcome to the greatest game you'll ever play. To start off, what is the name of your character?\n")
-print("Enjoy the journey, ", userName, "!")
-print("If at anytime you would like to quit the game, enter q or Q or quit when asked for input, otherwise type start to begin your adventure.")
+print("Enjoy the journey, ", userName, "!\n\n")
+print("If at anytime you would like to quit the game, enter q or Q or quit when asked for input.\n\n Otherwise type start to begin your adventure.\n\n\n")
 
 # This will be the Game loop.
 while gameIsOver == False:
@@ -35,4 +35,20 @@ while gameIsOver == False:
         delprint("Thanks for playing!")
 
     elif (userInput == "start" or userInput == "Start"):
-        delprint("And so we begin our tale...", .05)
+        delprint("\n\n\nAnd so we begin our tale...\n \n \n")
+        delprint(
+            f"The frigid rain pelts {userName}'s neck and back as he forces \nhimself to take another weary step through the sludge.\n\n")
+        delprint("\x1B[3mWhat's that up ahead?\x1B[0m, he thinks\n")
+        delprint(
+            f"Barely visible through the downpour, {userName} can just make out a dark shape rising from the mud.\n\n")
+        delprint(
+            f"Relief washes over him as he realizes what it is.\n\n \x1B[3mA signpost!\x1B[0m\n\n")
+
+        userInput = input("What do you want to do?")
+
+        if (userInput == "approach the signpost" or userInput == "read the signpost" or userInput == "approach" or userInput == "read"):
+            delprint(
+                "Despite being worn down by wind and time, the letters on the sign are still legible: \x1B[3m<Insert ool and creepy town name here>\x1B[0m\n\n\n")
+        else:
+            delprint(
+                "That doesn't seem to be an option right now. Try something else.")
