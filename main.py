07570a40f3c2d755277191461eae08fc5c679d14
stellernate <stellernate@gmail.com>
2023-02-26 14:53:12 -0600

    slow printing idea
    I added the delprint function to give it a scrolling text vibe. Might be too much of a pain though, because I'm not sure how to make it able to accept different numbers of arguments. On line 27 it reads it s three arguments rather than two. Not committed to this but let me know what you think.

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from Person import Person
+import sys
+import time
+
+# don't know if this is more hassle than it's worth but it's kind of fun
 
 
-#This class will be the class that will load the game and run the game.
-#The game will have a loop which will keep the game running. I'm thinking
-#we build the game from the ground up and make the game start off simple and 
-#we slowly add more and more.
+def delprint(text, delay_time=.05):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(delay_time)
 
-#initial setup
+
+# This class will be the class that will load the game and run the game.
+# The game will have a loop which will keep the game running. I'm thinking
+# we build the game from the ground up and make the game start off simple and
+# we slowly add more and more.
+
+# initial setup
 p1 = Person("John", 36)
 gameIsOver = False
 
- 
 
-userName = input("Welcome to the greatest game you'll ever play. To start off, what is the name of your character?\n")
+userName = input(
+    "Welcome to the greatest game you'll ever play. To start off, what is the name of your character?\n")
 print("Enjoy the journey, ", userName, "!")
-print("If at anytime you would like to quit the game, enter q or Q or quit when asked for input")
+print("If at anytime you would like to quit the game, enter q or Q or quit when asked for input, otherwise type start to begin your adventure.")
 
-#This will be the Game loop.
+# This will be the Game loop.
 while gameIsOver == False:
-  userInput = input("What would you like to do?")
-  if(userInput == "q" or userInput == "Q" or userInput == "quit"):
-    gameIsOver = True
-    print("Thanks for playing!")
+    userInput = input("What would you like to do? ")
+    if (userInput == "q" or userInput == "Q" or userInput == "quit"):
+        gameIsOver = True
+        delprint("Thanks for playing!")
 
-
-
-
+    elif (userInput == "start" or userInput == "Start"):
+        delprint("And so we begin our tale...", .05)
